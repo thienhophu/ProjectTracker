414f6615ba889490b05da4b6e8732a3c166e1138
Thien Ho <thien.hophu@gmail.com>
2022-04-11 10:26:23 +0700

    Updated

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'au.projecttracker.app',
-  appName: 'ProjectTracker',
+  appName: 'Project Tracker',
   webDir: 'build',
   bundledWebRuntime: false
 };
