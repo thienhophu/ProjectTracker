82574dc45c0c975654a6507199861c16d5659aee
Hieu Nguyen <dhieu0307@gmail.com>
2022-04-13 10:46:01 +0700

    dashboard-screen
    refactor

---
+++ src/data/projects.ts
@@ -0,0 +1,58 @@
+export interface Project {
+  id: number;
+  imageUrl: string;
+  title: string;
+  description: string;
+}
+
+export const projects: Project[] = [
+  {
+    title: 'Matt Chorsey',
+    imageUrl: 'https://www.linkpicture.com/q/mansion.jpg',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    id: 0
+  },
+  {
+    title: 'Lauren Ruthford',
+    imageUrl: 'https://www.linkpicture.com/q/mansion.jpg',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    id: 1
+  },
+  {
+    title: 'Jordan Firth',
+    imageUrl: 'https://www.linkpicture.com/q/mansion.jpg',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    id: 2
+
+  },
+  {
+    title: 'Bill Thomas',
+    imageUrl: 'https://www.linkpicture.com/q/mansion.jpg',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    id: 3
+  },
+  {
+    title: 'Joanne Pollan',
+    imageUrl: 'https://www.linkpicture.com/q/mansion.jpg',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    id: 4
+  },
+  {
+    title: 'Andrea Cornerston',
+    imageUrl: 'https://www.linkpicture.com/q/mansion.jpg',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    id: 5
+  },
+  {
+    title: 'Moe Chamont',
+    imageUrl: 'https://www.linkpicture.com/q/mansion.jpg',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    id: 6
+  },
+  {
+    title: 'Kelly Richardson',
+    imageUrl: 'https://www.linkpicture.com/q/mansion.jpg',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    id: 7
+  }
+];
