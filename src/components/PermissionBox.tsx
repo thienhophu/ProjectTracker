b5305f8e00ecf7fd9f53def3485e6007879271a0
Bao Vo <bao@manifera.com>
2022-05-06 09:02:42 +0700

    adjust codes

--- src/components/PermissionBox.tsx
+++ src/components/PermissionBox.tsx
@@ -3,13 +3,12 @@ import { useAppSelector, usePermissions } from '../app/hooks';
 const PermissionBox = ({
   children,
   has,
-}: React.PropsWithChildren<{ has: string }>): JSX.Element => {
+}: React.PropsWithChildren<{ has: string }>): JSX.Element | null => {
   const user = useAppSelector((state) => state.auth.userData);
-  console.log('🚀 ~ file: PermissionBox.tsx ~ line 8 ~ user', user);
   const hasPermission = usePermissions({ permission: has, user });
 
   if (!hasPermission) {
-    return <></>;
+    return null;
   }
 
   return children as JSX.Element;
