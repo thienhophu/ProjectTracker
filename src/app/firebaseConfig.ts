726ff7bc7882a1e81562339147b8d7a9b4a29444
Thien Ho <thien.hophu@gmail.com>
2022-06-30 23:55:26 +0700

    push notification for android

--- src/app/firebaseConfig.ts
+++ src/app/firebaseConfig.ts
@@ -1,6 +1,7 @@
 import { FirebaseApp, initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getMessaging, Messaging, getToken } from 'firebase/messaging';
 
 const FIREBASE_API_KEY = process.env.REACT_APP_FIREBASE_API_KEY;
 
@@ -18,5 +19,21 @@ const config = {
 const firebaseApp: FirebaseApp = initializeApp(config);
 const auth = getAuth(firebaseApp);
 const firestore = getFirestore(firebaseApp);
+const messaging: Messaging = getMessaging(firebaseApp);
+
+getToken(messaging, { vapidKey: 'BIthTVqq3HhrXEbu5aD_Vt143-VV4YmuSVeZDg9g71Rl4wWBqYIIUm1tKpjz0bDiH8ZuT3DU3Jg47aaIRIhA_l8' }).then((currentToken) => {
+  if (currentToken) {
+    console.log('🚀 ~ currentToken', currentToken)
+    // Send the token to your server and update the UI if necessary
+    // ...
+  } else {
+    // Show permission request UI
+    console.log('No registration token available. Request permission to generate one.');
+    
+  }
+}).catch((err) => {
+  console.log('An error occurred while retrieving token. ', err);
+  // ...
+});
 
 export { config, auth, firestore };
